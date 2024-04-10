9843658e3281ffd7f3c8b56beac2d27233b54059
micke259 <zbz9090@mail.ru>
2024-04-10 18:51:56 +0300

    review

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,76 @@ import './index.scss'
 import Swiper from 'swiper/bundle'
 import {SwiperOptions} from 'swiper/types'
 import { Navigation } from 'swiper/modules';
+import Inputmask from 'inputmask';
 
+
+
+
+
+const tabs = document.querySelectorAll('input[name="tab-btn"]')
+const month = document.getElementsByClassName("abonement__selector__item__tab")
+const priceTags = document.querySelectorAll(".courses__list__item__price__title")
+
+const priceValues = Array.from(priceTags).map(tag => parseInt(tag.textContent));
+const priceValues_2 = Array.from(priceTags).map(tag => parseInt(tag.textContent)*4);
+const priceValues_3 = Array.from(priceTags).map(tag => parseInt(tag.textContent)*7);
+
+
+tabs.forEach((tab, index) => {
+	tab.addEventListener("change", () => {
+		const months = parseInt(month[index].textContent)
+			switch (months) {
+				case 1:
+					priceTags.forEach((tag, index) =>{
+						priceTags[index].textContent = String(priceValues[index])
+					}) 
+					break;
+				case 6:
+					priceTags.forEach((tag, index) =>{
+						priceTags[index].textContent = String(priceValues_2[index])
+					}) 
+					break
+				case 12:
+					priceTags.forEach((tag, index) =>{
+						priceTags[index].textContent = String(priceValues_3[index])
+					}) 
+					break
+			}
+		
+	})
+})
 
 
 const CoachesParams:SwiperOptions = {
-	slidesPerView:4,
 	speed:200,
 	loop:true,
 	navigation: {
 		nextEl: '.swiper-button-next',
     	prevEl: '.swiper-button-prev',
   	},
+	breakpoints:{
+		1024:{
+			slidesPerView:4,
+			spaceBetween:40,
+		},
 
+		900:{
+			slidesPerView:3,
+		},
+
+		768:{
+			slidesPerView:2,
+			spaceBetween:30,
+		},
+
+		640:{
+			slidesPerView:2
+		},
+
+		320:{
+			slidesPerView:1
+		}
+	},
 	
 
 	watchOverflow:false,
@@ -24,11 +82,11 @@ const CoachesParams:SwiperOptions = {
 const swiper = new Swiper('.coaches__list', CoachesParams);
 
 const RepliesParams:SwiperOptions = {
-	slidesPerView:1,
 	loop:true,
+	speed:200,
 	navigation:{
-		nextEl: 'swiper-button-next',
-		prevEl: 'swiper-button-prev',
+		nextEl: '.advert-button-next',
+		prevEl: '.advert-button-prev',
 	},
 
 	watchOverflow:false,
@@ -36,3 +94,7 @@ const RepliesParams:SwiperOptions = {
 }
 
 const replies = new Swiper('.replies', RepliesParams)
+
+const phoneInput = document.querySelector<HTMLInputElement>('.free__inner__input[type="text"][placeholder="Телефон"]');
+
+Inputmask({ mask: '+7 (999) 999-99-99' }).mask(phoneInput);
